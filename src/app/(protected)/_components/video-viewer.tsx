4584d65856cff00299d22a3067b955e1e1d06851
walshs88 <walshs88@tcd.ie>
2026-02-09 11:05:54 +0000

    linting

--- src/app/(protected)/_components/video-viewer.tsx
+++ src/app/(protected)/_components/video-viewer.tsx
@@ -4,7 +4,6 @@ import type { Video } from "@/lib/video-type";
 import { Badge } from "@/components/ui/badge";
 import { Spinner } from "@/components/ui/spinner";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import {
   DropdownMenu,
   DropdownMenuContent,
